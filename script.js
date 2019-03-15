744e09c39be0f85916104964ae8aa35650fc6ce3
Kevin Toro <torok@torok-macbookpro.roam.corp.google.com>
2019-03-15 16:33:23 -0400

    finished questions

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ let submitBtn = document.getElementById("submitButton");
 submitBtn.addEventListener("click", getPersonality);
 
 function getPersonality(){
-    
+    //retrieve elements that contain responses
+    //assess whether response has numerical value to evaluate to a type of bird
+    //return the type of bird they are
 }
