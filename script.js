99a1862a7ba7bab96acfdffa3972c15127e42ff0
Kevin Toro <torok@torok-macbookpro.roam.corp.google.com>
2019-03-30 08:47:49 -0400

    adding boilerplate for jquery start

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 let submitBtn = document.getElementById("submitButton");
-submitBtn.addEventListener("click", getPersonality);
+//submitBtn.addEventListener("click", getPersonality);
+
+//Declare three variables representing question category
+let activity = null;
+let diet = null;
+let flight = null;
+
+$(document).ready(function(){
+    //all JS methods will go inside here
+
+});
+    
 
 function getPersonality(){
+    console.log("get personality has been called");
     //retrieve elements that contain responses
     //assess whether response has numerical value to evaluate to a type of bird
     //return the type of bird they are
+}
+
+
+function resetQuiz(){
+    console.log("reset quiz has been called");
+}
+
+function processResults(){
+    console.log("process Results has been called");
 }
